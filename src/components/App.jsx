d90742f96af53c8775648e86f33b9f3fa0271d70
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-08-27 14:43:39 +0530

    Add Notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-import notes from "../notes";
-import Footer from "./Footer";
+import React, { useState } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-function App(){
-    return (
-        <div>
-            <Header/>
-            {notes.map( note => (
-                <Note 
-                    key = {note.key}
-                    title = {note.title} 
-                    content = {note.content}
-                />
-            ))}
-            <Footer/>
-        </div>
-    );
+function App() {
+
+    const [notes, setNotes] = useState([]);
+    function addNote(note){
+        setNotes(prevNotes => {
+            return [...prevNotes,note];
+        });
+    }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd = {addNote}/>
+      {notes.map( (note,index) => {
+        return <Note key={index} title={note.title} content={note.content} />
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
